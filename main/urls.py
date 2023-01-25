2ff7ecb74555be66a9f8637261b4fb7e9462cb37
oh-bom <spdlqj888888@gmail.com>
2023-01-26 00:49:52 +0900

    찐 최종

--- main/urls.py
+++ main/urls.py
@@ -6,7 +6,7 @@ app_name='main'
 
 urlpatterns = [
     path('',views.post_list,name='post_list'),
-    path('create',views.post_create,name='post_create'),
+    path('create/',views.post_create,name='post_create'),
     path('like_ajax',views.like_ajax,name='like_ajax'),
     path('comment_create',views.comment_create,name='comment_create'),
     path('comment_delete',views.comment_delete,name='comment_delete'),
